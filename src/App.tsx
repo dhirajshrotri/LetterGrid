5d893dff7499252183d657b95a654b858caa4266
Dhiraj Shrotri <dhiraj.shrotri@truweathersolutions.com>
2026-02-23 16:07:57 -0500

    feat: added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import LetterGrid from './Components/LetterGrid'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div style={{
       width: "100vw"
     }}>
-<LetterGrid />
+      <LetterGrid />
+      <Analytics />
     </div>
       
   )
